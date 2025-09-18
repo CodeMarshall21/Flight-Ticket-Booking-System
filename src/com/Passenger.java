e6939dc1dffb957041350c15ec41b27ede596107
keshav <k21keshav05@gmail.com>
2025-09-18 23:01:22 +0530

    Passenger.java

--- src/com/Passenger.java
+++ src/com/Passenger.java
@@ -1,4 +1,24 @@
 package com;
 
 public class Passenger {
+    private String bookingId;
+
+    private String name;
+
+    private int age;
+
+    private int seatsBooked;
+
+    public String getBookingId(){
+        return bookingId;
+    }
+
+    public int getSeatsBooked(){
+        return seatsBooked;
+    }
+
+    @Override
+    public String toString(){
+        return "Passenger{ Booking ID: " + bookingId + ", Name: " + name + ", Age: " + age + ", Seats Booked: " + seatsBooked + " }";
+    }
 }
